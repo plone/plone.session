daced6757cc733534388aa2470db7e75c27ba495
Wichert Akkerman <wichert@wiggy.net>
2006-10-14 15:49:58 +0000

    Add a SHA1 hash based session plugin
    svn path=/plone.session/trunk/; revision=11155

---
+++ plone/session/adapters/hash.py
@@ -0,0 +1,55 @@
+from zope.interface import implements
+from zope.annotation.interfaces import IAnnotations
+from plone.session.interfaces import ISessionSource
+import hmac, sha
+
+def GenerateSecret(length=16):
+    letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letters+="abcdefghijklmnopqrstuvwxyz"
+    letters+="01234567890!@#$%^&*()"
+
+    secret=""
+    for i in range(length):
+        secret+=random.choice(letters)
+
+    return secret
+
+
+class HashSession(object):
+    implements(ISessionSource)
+
+    def __init__(self, context):
+        self.context=context
+        anno=IAnnotations(self.context)
+        if not anno.has_key("plone.session.plugins.hash.secret")
+            anno["plone.session.plugins.hash.secret"]=GenerateSecret()
+
+
+    def getSecret(self):
+        anno=IAnnotations(self.context)
+        return anno["plone.session.plugins.hash.secret"]
+
+
+    def signUserid(self, userid):
+        return hmac.new(self.getSecret(), userid, sha).digest()
+
+
+    def createIdentifier(self, userid):
+        signature=self.signUserid(userid)
+
+        return "%s %s" % (signature, userid)
+
+
+    def verifyIdentitier(self, identifier):
+        (signature, userid)=identifier.split()
+        return signature==self.signUserid(userid)
+
+
+    def extractUserid(self, identifier):
+        (signature, userid)=identifier.split()
+        return userid
+
+
+    def invalidateSession(self, principal):
+        pass
+
