0d8c47449279d9f69c3b0bd3273334f5ee222b7b
Wichert Akkerman <wichert@wiggy.net>
2006-10-25 18:32:16 +0000

    Load some ZCML before running ourtests. This makes sure our adapters are properly registered
    svn path=/plone.session/trunk/; revision=11218

--- plone/session/tests/testPAS.py
+++ plone/session/tests/testPAS.py
@@ -3,13 +3,27 @@ if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
 
 from Testing import ZopeTestCase
+from Testing.ZopeTestCase.placeless import setUp, tearDown
+from Testing.ZopeTestCase.placeless import zcml
 from plone.session.plugins.session import SessionPlugin
 from plone.session.interfaces import ISessionPlugin, ISessionSource
+import Products.Five.tests
+import plone.session
 
 
 class TestOpenIdExtraction(ZopeTestCase.ZopeTestCase):
     def afterSetUp(self):
+        setUp()
+	zcml.load_config('meta.zcml', Products.Five)
+	zcml.load_config('permissions.zcml', Products.Five)
+	zcml.load_config('directives.zcml', Products.Five.tests)
+	zcml.load_config('configure.zcml', plone.session)
         self.app._setObject("session", SessionPlugin("session"))
+
+
+    def beforeTeearDown():
+        tearDown()
+
 
     def testInterfaces(self):
         self.assertEqual(ISessionPlugin.providedBy(self.app.session), True)
