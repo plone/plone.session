fa5df07a65b8af5aaf0fb01f21c3075bf578155b
Stefan H. Holek <stefan@epy.co.at>
2007-03-30 15:31:38 +0000

    Make tests pass by using FunctionalPloneSessionTestCase also for PAS tests. Dunno really why this is necessary. Something in testExtraction causes a commit under certain circumstances ... perhaps.
    svn path=/plone.session/trunk/; revision=13999

--- plone/session/tests/sessioncase.py
+++ plone/session/tests/sessioncase.py
@@ -1,5 +1,5 @@
 from Testing import ZopeTestCase
-from Testing.ZopeTestCase.placeless import zcml
+from Products.Five import zcml
 
 import plone.session
 from plone.session.plugins.session import SessionPlugin
@@ -14,23 +14,9 @@ class PloneSessionTestCase(ZopeTestCase.ZopeTestCase):
 
     def afterSetUp(self):
         zcml.load_config('configure.zcml', plone.session)
-        if not self.app.hasObject("folder"):
-            self.app._setObject("folder", Folder("folder"))
-        if not self.app.folder.hasObject("session"):
-            self.app.folder._setObject("session", SessionPlugin("session"))
+        self.folder._setObject("session", SessionPlugin("session"))
 
 
 class FunctionalPloneSessionTestCase(ZopeTestCase.Functional, PloneSessionTestCase):
-    def afterSetUp(self):
-        import Products.Five.tests
-        zcml.load_config('meta.zcml', Products.Five)
-        zcml.load_config('permissions.zcml', Products.Five)
-        zcml.load_config('directives.zcml', Products.Five.tests)
-
-        zcml.load_config('configure.zcml', plone.session)
-
-        from plone.session.plugins.session import SessionPlugin
-        session=SessionPlugin("session")
-        self.folder._setObject("session", session)
-
+    pass
 
--- plone/session/tests/testPAS.py
+++ plone/session/tests/testPAS.py
@@ -1,31 +1,29 @@
 from zope.publisher.browser import TestRequest
 from plone.session.interfaces import ISessionPlugin, ISessionSource
 import plone.session
-from sessioncase import PloneSessionTestCase
+from sessioncase import FunctionalPloneSessionTestCase
 
 
-class TestSessionPlugin(PloneSessionTestCase):
+class TestSessionPlugin(FunctionalPloneSessionTestCase):
 
     def testInterfaces(self):
-        session = self.app.folder.session
+        session = self.folder.session
         self.assertEqual(ISessionPlugin.providedBy(session), True)
         source = session.source
         self.assertEqual(ISessionSource.providedBy(source), True)
 
-
     def makeRequest(self, cookie):
-        session = self.app.folder.session
+        session = self.folder.session
         return TestRequest(**{session.cookie_name : cookie})
 
-
     def testExtraction(self):
-        session = self.app.folder.session
+        session = self.folder.session
 
         request=self.makeRequest("test string".encode("base64"))
         creds=session.extractCredentials(request)
         self.assertEqual(creds["source"], "plone.session")
         self.assertEqual(creds["cookie"], "test string")
-        
+
         request=self.makeRequest("test string")
         creds=session.extractCredentials(request)
         self.assertEqual(creds, {})
