2f0688f878ffa59539ae8984296a9458afc08543
Wichert Akkerman <wichert@wiggy.net>
2007-06-03 15:31:16 +0000

    Add updateCredentials to FakePAS
    svn path=/plone.session/trunk/; revision=15176

--- plone/session/tests/sessioncase.py
+++ plone/session/tests/sessioncase.py
@@ -11,6 +11,10 @@ from OFS.Folder import Folder
 class FakePAS(Folder):
     plugins = None
 
+    def updateCredentials(self, request, response, userid, password):
+        self.credentials=(userid, password)
+
+
     def _verifyUser(self, plugin, user_id=None, login=None):
         assert user_id is None
         if login=='our_user':
