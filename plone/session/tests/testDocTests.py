f576e0009facf1bd70b3b89d09e5984199766ba7
Hanno Schlichting <hanno@hannosch.eu>
2010-05-23 12:02:49 +0000

    Use the standard libraries doctest module, instead of the deprecated one from zope.testing.
    svn path=/plone.session/trunk/; revision=36701

--- plone/session/tests/testDocTests.py
+++ plone/session/tests/testDocTests.py
@@ -1,14 +1,12 @@
+import doctest
 import unittest
-from zope.testing import doctest
 
 from plone.session import tktauth
 
-optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
+optionflags = doctest.ELLIPSIS
 
 
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(doctest.DocTestSuite(tktauth,
-            optionflags=optionflags))
-
+    suite.addTest(doctest.DocTestSuite(tktauth, optionflags=optionflags))
     return suite
