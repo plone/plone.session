e1f053b478def2ac87944da136b3e226d1e65847
Matthew Wilkes <git@matthewwilkes.name>
2012-11-02 00:44:20 +0000

    Typo: Add missing :, we want the first 4 bytes, not the 4th byte. That'll teach me not to push too quickly.

--- plone/session/tktauth.py
+++ plone/session/tktauth.py
@@ -156,7 +156,7 @@ def createTicket(secret, userid, tokens=(), user_data='', ip='0.0.0.0', timestam
     # Unfortunately, some older versions of Python assume that longs are always
     # 32 bits, so we need to trucate the result in case we are on a 64-bit
     # naive system.
-    data1 = inet_aton(ip)[4] + pack("!I", timestamp)
+    data1 = inet_aton(ip)[:4] + pack("!I", timestamp)
     data2 = '\0'.join((userid, token_list, user_data))
     if mod_auth_tkt:
         digest = mod_auth_tkt_digest(secret, data1, data2)
