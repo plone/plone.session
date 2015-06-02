1572ff11cc065fa51e236608ce8188d801b47129
Jens W. Klein <jk@kleinundpartner.at>
2015-06-02 14:30:46 +0200

    indent, other minor cleanup

--- setup.py
+++ setup.py
@@ -1,46 +1,51 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
 
 version = '3.5.6.dev0'
+longdescription = open("README.rst").read()
+longdescription += '\n'
+longdescription += open("CHANGES.rst").read()
 
-setup(name='plone.session',
-      version=version,
-      description="Session based authentication for Zope",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Plone :: 4.3",
-          "Framework :: Plone :: 5.0",
-          "Framework :: Zope2",
-          "License :: OSI Approved :: BSD License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.7",
-          "Topic :: System :: Systems Administration :: Authentication/Directory",
-        ],
-      keywords='PAS session authentication Zope',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://pypi.python.org/pypi/plone.session',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      extras_require=dict(
-        test=[
-            'zope.configuration',
-            'zope.publisher',
-        ]
-      ),
-      install_requires=[
-        'setuptools',
-        'plone.keyring',
-        'plone.protect',
-        'zope.component',
-        'zope.interface',
-        'Products.PluggableAuthService',
-        'Zope2',
-      ],
-      )
+setup(
+    name='plone.session',
+    version=version,
+    description="Session based auth tkt authentication for Zope",
+    long_description=longdescription,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python",
+        "Topic :: System :: Systems Administration :: Authentication/Directory",  # noqa
+    ],
+    keywords='PAS session authentication Zope auth_tkt',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://pypi.python.org/pypi/plone.session',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=dict(
+      test=[
+          'zope.configuration',
+          'zope.publisher',
+      ]
+    ),
+    install_requires=[
+      'plone.keyring',
+      'plone.protect',
+      'Products.PluggableAuthService',
+      'setuptools',
+      'zope.component',
+      'zope.interface',
+      'Zope2',
+    ],
+)
