15d85db4a12d5dc65ae72c9ac4b4794d0e6f1399
Jens W. Klein <jk@kleinundpartner.at>
2018-08-17 10:45:56 +0200

    update troove classifier

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python',
         'Topic :: System :: Systems Administration :: Authentication/Directory',  # noqa
     ],
