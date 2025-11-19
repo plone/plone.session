ac6afe3daed215822b8ac25321a060a644854239
Maurits van Rees <maurits@vanrees.org>
2025-11-19 10:42:08 +0100

    Bumped version for breaking release.
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-version = "5.0.0.dev0"
+version = "5.0.0a1.dev0"
 longdescription = open("README.rst").read()
 longdescription += "\n"
 longdescription += open("CHANGES.rst").read()
